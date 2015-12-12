b955e45a300567b5950fcbaa7740272a3f9292a0
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-12-12 12:44:48 -0600

    finished day 3

--- 03/part2.js
+++ 03/part2.js
@@ -2,15 +2,14 @@ var fs = require("fs");
 var input = fs.readFileSync("./input.txt", "utf8");
 
 // both start by giving gift at (0,0)
-var santaHomes = { '0,0': 1 };
-var roboSantaHomes = { '0,0': 1 };
+var masterHomesObj = { '0,0': 1 };
 var x = 0, y = 0;
 var a = 0, b = 0;
 
 for (var i = 0; i < input.length; i++) {
 
   // only the normal santa moves:
-  if (input[i] % 2) {
+  if (i%2 == 0) {
     switch (input[i]) {
       case '<':
         y--;
@@ -29,8 +28,8 @@ for (var i = 0; i < input.length; i++) {
     // for each move, record the grid location:
     normalSanta = x + ',' + y;
 
-    if (!santaHomes[normalSanta]) {
-      santaHomes[normalSanta] = 1;
+    if (!masterHomesObj[normalSanta]) {
+      masterHomesObj[normalSanta] = 1;
     }
   }
 
@@ -54,12 +53,13 @@ for (var i = 0; i < input.length; i++) {
     // for each move, record the grid location:
     roboSanta = a + ',' + b;
 
-    if (!roboSantaHomes[roboSanta]) {
-      roboSantaHomes[roboSanta] = 1;
+    if (!masterHomesObj[roboSanta]) {
+      masterHomesObj[roboSanta] = 1;
     }
   }
 
-  console.log(normalSanta);
 }
 
-console.log(Object.keys(santaHomes).length);
+console.log(Object.keys(masterHomesObj).length);
+
+// NEED TO CHECK MASTER LIST OF LOCATIONS
