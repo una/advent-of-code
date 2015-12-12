14c788f0fa03b8514b25e88c37a277d8f683bd20
UNA A. KRAVETS <uakravet@us.ibm.com>
2015-12-12 13:03:51 -0600

    day 4 finished! yayy

---
+++ 04/part2.js
@@ -0,0 +1,19 @@
+var crypto = require('crypto');
+var input = "bgvyzdsv";
+var found = false;
+
+// keep creating hashes until we find one that starts with "00000"
+for(var i=0; found != true ; i++){
+
+  var hash = crypto.createHash('md5');
+  var inputHashed = input + i;
+  hash.update(inputHashed);
+
+  var newHash = hash.digest('hex');
+
+  // if the hash starts with "00000"
+  if(newHash.slice(0,6)==="000000"){
+    found = true;
+    console.log(i);
+  }
+}
